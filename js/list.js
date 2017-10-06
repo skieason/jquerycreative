00840197b1ab731f5311f8333cb15e5cfba96a93
Phil Varner <philvarnerlp9@gmail.com>
2017-10-06 02:32:16 -0600

    asdf

--- js/list.js
+++ js/list.js
@@ -1,6 +1,25 @@
+String.prototype.repeat = function(num){
+	return new Array(num + 1).join(this);
+};
+
+var filter = ['ass', 'piss', 'hell', 'fuck', 'You could see there was blood coming out of her eyes. Blood coming out of her wherever.'];
+
+var filterIt = function (txt) {
+	for(var i=0; i<filter.length; i++){
+		// Create a regular expression and make it global
+		var pattern = new RegExp('\\b' + filter[i] + '\\b', 'g');
+
+		// Create a new string filled with '*'
+		var replacement = '*'.repeat(filter[i].length);
+
+		txt = txt.replace(pattern, replacement);
+	}
+	// returning txt will set the new text value for the current element
+	return txt;
+};
 
 $('input').bind("enterKey", function (e){
-	$('ul').append('<li id="one">' + $("#input-text").val() + '</li>');
+	$('#list2').prepend('<li id="one">' + $("#input-text").val() + '</li>');
 	$("#input-text").val("")
 });
 
@@ -11,23 +30,37 @@ $('input').keydown(function(e){
 });
 
 $('ul').on('click', 'li', function () {
+	$('#list2').append('<li id="one">' + $(this).text() + '</li>');
 	$(this).remove();
+});
+
+setInterval(function(){
+	console.log('whatup');
+	$.get("https://api.whatdoestrumpthink.com/api/v1/quotes/random", function(data, status){
+		data.message = filterIt(data.message);
+		$('#list').append('<li id="one">' + data.message + '</li>');
+	});
+}, 4000);
+
+$('#yes').click(function () {
+	$('#title2').text('Click the quotes you like on the left to see them over here!');
+	$(this).parent().remove();
+});
+
+$('#no').click(function () {
+	$('#title2').text('Click the quotes you hate on the left so we can have the satisfaction of deleting them all at once when you\'re ready');
+	$('#delete').text('Button of Destruction');
+	$(this).parent().remove();
+});
+
+$('#delete').click(function () {
+	$('#list2').empty();
 });
 
 $('#button').click(function () {
 	$.ajax({
-		type: "POST",
-		dataType: "jsonp",
-		// username: "ACdad705d8a707b135c57108150fb1d6ee", // account_sid
-		// password: "302856736009a067774e8a0f93d0a67e", // auth token
-		url: "https://rest.nexmo.com/sms/json",
-		data: {
-			api_key: "a6e22fe2",
-			api_secret: "cac2825ea7aa4645",
-			to: "+911234567890",
-			from: "Nexmo",
-			text: "From jQuery Ajax"
-		},
+		type: "GET",
+		url: "https://api.whatdoestrumpthink.com/api/v1/quotes/random",
 		success: function(data) {
 			console.log(data);
 		},
